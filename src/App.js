78509aa0b71bec40e64c6e4e6d9b0c076488f100
Lyndon Rey Bualat <lyndonreybualat@gmail.com>
2022-10-31 00:11:54 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App"> 
-      <h1>Hello</h1>
+      <Navbar/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div className="Main"> 
+      
+    </div>
+  );
+}
+
+export default Main;
